1c044ee3c0dd25ce778997658058182f5f7af8fa
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2024-01-24 22:01:02 -0400

    ConfigModule no Nest.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CreateAccountController } from './controllers/create-account.controller';
+import { envSchema } from './env';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    validate: env => envSchema.parse(env),
+    isGlobal: true,
+  }),],
   controllers: [CreateAccountController,],
   providers: [PrismaService,],
 })
