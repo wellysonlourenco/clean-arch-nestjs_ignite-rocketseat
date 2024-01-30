ef3777a8645df9c4489b4b4a35222dcd14b044a7
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2024-01-29 21:41:47 -0400

    Controller de listagem de perguntas com paginação

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from './auth/auth.module';
 import { AuthenticateController } from './controllers/authenticate-controller';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { CreateQuestionController } from './controllers/create-question.controller';
+import { FetchRecentQuestionController } from './controllers/fetch-recent-questions.controller';
 import { envSchema } from './env';
 import { PrismaService } from './prisma/prisma.service';
 
@@ -20,6 +21,7 @@ import { PrismaService } from './prisma/prisma.service';
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
+    FetchRecentQuestionController,
   ],
   providers: [PrismaService,],
 })
---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Query, UseGuards } from "@nestjs/common"
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard"
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe"
+import { PrismaService } from "src/prisma/prisma.service"
+import { z } from "zod"
+
+
+const pageQueryParamSchema = z
+    .string()
+    .optional()
+    .default('1')
+    .transform(Number)
+    .pipe(z.number().min(1))
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const queryValidatioPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionController {
+    constructor(private prisma: PrismaService) { }
+
+    @Get()
+    async handle(@Query('page', queryValidatioPipe) page: PageQueryParamSchema  ) {
+        const perPage = 1
+
+
+        const question = await this.prisma.question.findMany({
+            take: 1,
+            skip: (page -1) * perPage,
+            orderBy: {
+                createdAt: "desc",
+            },
+        })
+
+        return question
+    }
+}
