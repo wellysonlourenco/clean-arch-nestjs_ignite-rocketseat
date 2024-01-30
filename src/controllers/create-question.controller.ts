efa56f2e9bc9f21c7ca6006519125dd7a64f7b6d
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2024-01-29 20:33:15 -0400

    decorator de autenticação

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, UseGuards } from "@nestjs/common"
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard"
+import { TokenPayload } from "src/auth/jwt.strategy"
 import { PrismaService } from "src/prisma/prisma.service"
+import { CurrentUser } from "./current-user-decorator"
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
@@ -8,7 +10,8 @@ export class CreateQuestionController {
     constructor(private prisma: PrismaService) { }
 
     @Post()
-    async handle() {
+    async handle(@CurrentUser() user: TokenPayload) {
+        console.log(user)
         return 'ok'
     }
 
